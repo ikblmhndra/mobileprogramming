0713e09d4b634251a48222b6e7c06e39f70e3b23
ikblmhndra <ikbalmahendra.96@gmail.com>
2023-04-01 01:01:07 +0700

    KonversiSuhu

---
+++ app/src/main/java/com/nameless/mobileprogramming/perhitungan/KonversiSuhu.java
@@ -0,0 +1,16 @@
+package com.nameless.mobileprogramming.perhitungan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nameless.mobileprogramming.R;
+
+public class KonversiSuhu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konversi_suhu);
+    }
+}
