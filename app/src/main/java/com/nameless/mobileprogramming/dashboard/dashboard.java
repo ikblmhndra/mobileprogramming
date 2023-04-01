a92f64b286047370c24963573a1da0c646964e63
ikblmhndra <ikbalmahendra.96@gmail.com>
2023-04-01 10:57:17 +0700

    Initial Commit

---
+++ app/src/main/java/com/nameless/mobileprogramming/dashboard/dashboard.java
@@ -0,0 +1,16 @@
+package com.nameless.mobileprogramming.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nameless.mobileprogramming.R;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
