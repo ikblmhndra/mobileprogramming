eaa48e65c75e46a67e764ac973be684f453e6783
ikblmhndra <ikbalmahendra.96@gmail.com>
2023-04-04 19:05:44 +0700

    Initial Commit

--- app/src/main/java/com/nameless/mobileprogramming/perhitungan/KonversiSuhu.java
+++ app/src/main/java/com/nameless/mobileprogramming/perhitungan/KonversiSuhu.java
@@ -9,10 +9,15 @@ import android.widget.Button;
 
 import com.nameless.mobileprogramming.R;
 import com.nameless.mobileprogramming.perhitungan.konversi.CelsiusToFahrenheit;
+import com.nameless.mobileprogramming.perhitungan.konversi.CelsiusToKelvin;
+import com.nameless.mobileprogramming.perhitungan.konversi.FahrenheitToCelsius;
+import com.nameless.mobileprogramming.perhitungan.konversi.FahrenheitToKelvin;
+import com.nameless.mobileprogramming.perhitungan.konversi.KelvinToCelsius;
+import com.nameless.mobileprogramming.perhitungan.konversi.KelvinToFahrenheit;
 
 public class KonversiSuhu extends AppCompatActivity {
 
-    Button btnCtoF;
+    Button btnCtoF, btnFtoC, btnCtoK, btnKtoC, btnFtoK, btnKtoF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,56 @@ public class KonversiSuhu extends AppCompatActivity {
         setContentView(R.layout.activity_konversi_suhu);
 
         btnCtoF = findViewById(R.id.btnCtoF);
+        btnFtoC = findViewById(R.id.btnFtoC);
+        btnCtoK = findViewById(R.id.btnCtoK);
+        btnKtoC = findViewById(R.id.btnKtoC);
+        btnFtoK = findViewById(R.id.btnFtoK);
+        btnKtoF = findViewById(R.id.btnKtoF);
+
         btnCtoF.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(KonversiSuhu.this, CelsiusToFahrenheit.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFtoC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KonversiSuhu.this, FahrenheitToCelsius.class);
+                startActivity(intent);
+            }
+        });
+
+        btnCtoK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KonversiSuhu.this, CelsiusToKelvin.class);
+                startActivity(intent);
+            }
+        });
+
+        btnKtoC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KonversiSuhu.this, KelvinToCelsius.class);
+                startActivity(intent);
+            }
+        });
+
+        btnFtoK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KonversiSuhu.this, FahrenheitToKelvin.class);
+                startActivity(intent);
+            }
+        });
+
+        btnKtoF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(KonversiSuhu.this, KelvinToFahrenheit.class);
                 startActivity(intent);
             }
         });
